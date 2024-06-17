525238da5747f099e0505e5fd3e6226f798eb94b
Jose <josebongo73@gmail.com>
2024-06-17 22:26:55 +0100

    test: testing api car wash

--- car-wash/src/main/java/com/agendei/projetoagendei/dtos/carWash/CarWashRecordDto.java
+++ car-wash/src/main/java/com/agendei/projetoagendei/dtos/carWash/CarWashRecordDto.java
@@ -3,7 +3,7 @@ package com.agendei.projetoagendei.dtos.carWash;
 import jakarta.validation.constraints.NotBlank;
 
 public record CarWashRecordDto(
-        @NotBlank String patientId,
+        @NotBlank String carWashId,
         @NotBlank String dateMedicalAppointment,
         @NotBlank String hourMedicalAppointment,
         @NotBlank String medicalAppointmentState
